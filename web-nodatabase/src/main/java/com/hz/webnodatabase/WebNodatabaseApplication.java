462d69e10ac69c038eaae0ecbe8091a89ffff3af
hz <1207732400@qq.com>
2019-10-14 10:51:25 +0800

    快速创建springboot的web项目，导入thymeleaf依赖 localhost:8080/hello测试成功

---
+++ web-nodatabase/src/main/java/com/hz/webnodatabase/WebNodatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.hz.webnodatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNodatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebNodatabaseApplication.class, args);
+    }
+
+}
---
+++ web-nodatabase/src/main/java/com/hz/webnodatabase/controller/helloController.java
@@ -0,0 +1,15 @@
+package com.hz.webnodatabase.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class helloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World.";
+    }
+}
